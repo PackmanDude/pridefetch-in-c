498915e9dd8317b2d2d83ea9a5c1876247571c19
PackmanDude <packmandude@protonmail.com>
2023-02-14 22:23:47 +0200

    HARD work in progress

--- pridefetch.c
+++ pridefetch.c
@@ -5,51 +5,78 @@
 #include <stdint.h>
 
 #define MAX_ROWS 4
+#define FLAGS_COUNT 2
 
 typedef uint_least8_t Flag[MAX_ROWS];
 
-const Flag GLORY_TO_UKRAINE = {33, 33, 226, 226};
-const Flag UPA = {88, 88, 0, 0};
-
 enum DrawAt {bg, fg};
 
-// I'm too biased to do it without chadstr
+void _fill_htab(void);
 str color256(int color, enum DrawAt bg_fg);
-
 void draw_info(const char *flag_name);
 
 int main(int argc, char *argv[])
 {
-	if (argc > 1)
+	if (argc < 2)
 	{
-		hcreate(32);
-		draw_info(argv[1]);
-		hdestroy();
-		return EXIT_SUCCESS;
+		printf("usage: %s <flag_name> (case-insensitive)\n", argv[0]);
+		return EXIT_FAILURE;
 	}
-	printf("usage: %s <flag_name> (case-insensitive)\n", argv[0]);
-	return EXIT_FAILURE;
+
+	hcreate(32);
+	_fill_htab();
+	draw_info(argv[1]);
+	hdestroy();
+	return EXIT_SUCCESS;
+}
+
+/* Mostly a technical function.
+	It fills hash table at start of the program. Later may be
+	implemented in completely different way or removed completely.
+*/
+void _fill_htab(void)
+{
+	const char *flags_names[FLAGS_COUNT];
+	flags_names[0] = "GLORY_TO_UKRAINE";
+	flags_names[1] = "UPA";
+	//
+	const Flag flags_colors[FLAGS_COUNT][MAX_ROWS] =
+	{
+		{33, 33, 226, 226},
+		{88, 88, 0, 0}
+	};
+
+	ENTRY entry, *pEntry = &entry;
+
+	for (int i = 0; i < FLAGS_COUNT; ++i)
+	{
+		pEntry->key = flags_names[i];
+		pEntry->data = malloc(sizeof(*flags_colors[0]) * MAX_ROWS);
+		memcpy(pEntry->data, flags_colors[i],
+			sizeof(*flags_colors[0]) * MAX_ROWS);
+
+		hsearch(*pEntry, ENTER);
+	}
+	free(pEntry->data);
 }
 
 str color256(int color, enum DrawAt bg_fg)
 {
+	// Check if in range
 	assert(color >= 0 && color <= 255 && bg_fg >= bg && bg_fg <= fg);
-	//char *ascii_code = malloc(sizeof(*ascii_code)*16);
+
 	// for background
 	if (!bg_fg)
 	{
-	// Maybe, some sunny day… I'll rewrite it.
-	/*	snprintf(ascii_code, sizeof(*ascii_code)*16, "%s%d%s",
-			"\033[48;5;", color, "m");
-	*/	chadstr ascii_code = str("\033[48;5;", color, "m");
+		chadstr ascii_code = str("\033[48;5;", color, "m");
 		return ascii_code;
 	}
 	// for foreground
 	if (bg_fg)
 	{
-	/*	snprintf(ascii_code, sizeof(*ascii_code)*16, "%s%d%s",
-			"\033[38;5;", color, "m");
-	*/	chadstr ascii_code = str("\033[38;5;", color, "m");
+		chadstr ascii_code = str("\033[38;5;", color, "m");
+									// ^ here is the difference
+									// shows correctly with 4 tab size
 		return ascii_code;
 	}
 	return NULL;
@@ -62,18 +89,22 @@ void draw_info(const char *flag_name)
 	const size_t len = strlen(flag_name);
 
 	char flag_name_all_caps[len];
+	char *pFlag_name_all_caps = flag_name_all_caps;
 	for (size_t i = 0; i < len; ++i)
 	{
-		// TODO: Make it pointer to a string
-		flag_name_all_caps[i] = toupper(flag_name[i]);
+		pFlag_name_all_caps[i] = toupper((unsigned char)flag_name[i]);
 	}
-	flag_name_all_caps[len] = '\0';
+	pFlag_name_all_caps[len] = '\0';
 
 	ENTRY entry, *pEntry = &entry;
+	pEntry->key = strdup(pFlag_name_all_caps);
 
-	pEntry->key = malloc(len + 1);
-	strcpy(pEntry->key, flag_name_all_caps);
-
+	/* TODO:
+	 ~~	Fill somehow hash table somewhere, where grass is nice
+	 ~~	and flowers shine at sunrise. Tip: global scope isn't
+	 ~~	that place.
+	   - I will, father.
+	*/
 	if (hsearch(*pEntry, FIND) == NULL)
 	{
 		fprintf(stderr, "'%s' was not found.\n", flag_name);
@@ -82,10 +113,9 @@ void draw_info(const char *flag_name)
 	}
 
 	Flag colors;
-	memcpy(colors, hsearch(*pEntry, FIND)->data, sizeof(colors));
-	free(pEntry->key);
+//	memcpy(colors, hsearch(*pEntry, FIND)->data, sizeof(colors));
 
-	uint_least8_t width = 20;
+	uint_least8_t width = 20; // characters
 	uint_least8_t current_row = 0;
 /*
 printf("Displaying contents of %s:\n", flag_name);
@@ -123,5 +153,8 @@ for (size_t i = 0; i < sizeof(colors) / sizeof(colors[0]); ++i)
 	for (size_t row = 0; row < strlen(flag); ++row)
 	{
 	}
-*/}
+*/
 
+	free(pEntry->key);
+//	free(pEntry->data);
+}
