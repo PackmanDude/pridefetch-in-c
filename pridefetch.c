88c15e1755a0b7b698aa48bc0dffce6f12e989c4
PackmanDude <packmandude@protonmail.com>
2023-07-06 13:09:43 +0300

    Update

--- pridefetch.c
+++ pridefetch.c
@@ -15,7 +15,7 @@ typedef struct
 enum DrawAt {bg, fg};
 
 void _fill_htab(void);
-str color256(uint_least8_t color, enum DrawAt bg_fg);
+str color256(enum DrawAt bg_fg, uint_least8_t color);
 void draw_info(const char *flag_name, const char *argv[]);
 
 int main(int argc, char *argv[])
@@ -29,8 +29,7 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	// str() is an object string. so we can compare them like that
-	if (str(argv[1]) == str("-h") || str(argv[1]) == str("--help"))
+	if (strcmp(argv[1], "-h") || strcmp(argv[1], "--help"))
 	{
 		printf("%s\n", str(*help_msg));
 		return EXIT_SUCCESS;
@@ -52,39 +51,17 @@ void _fill_htab(void)
 	Flag flag1 = { "GLORY_TO_UKRAINE", {33, 33, 226, 226} };
 	Flag flag2 = { "UPA", {88, 88, 0, 0} };
 
-	// Define them at compile time
 	ENTRY entry = { strdup(flag1.name), (void*)&flag1.rows };
-	// Note: .rows was a const pointer, we got rid of that
 	hsearch(entry, ENTER);
 	entry.key = strdup(flag2.name);
 	entry.data = (void*)&flag2.rows;
 	hsearch(entry, ENTER);
-
-	// meh
-	free(entry.key);
-//	free(entry.data);
 }
 
-str color256(uint_least8_t color, enum DrawAt bg_fg)
+str color256(enum DrawAt bg_fg, uint_least8_t color)
 {
-	// Check if in range
 	assert(bg_fg >= bg && bg_fg <= fg);
-
-	// for background
-	if (!bg_fg)
-	{
-		chadstr ascii_code = str("\033[48;5;", color, "m");
-		return ascii_code;
-	}
-	// for foreground
-	if (bg_fg)
-	{
-		chadstr ascii_code = str("\033[38;5;", color, "m");
-									// ^ here is the difference
-								// shows correctly with 4 tab size
-		return ascii_code;
-	}
-	return NULL;
+	return str("\033[", 4 - bg_fg, "8;5;", color, "m");
 }
 
 // My lil (non)bloat func
@@ -132,47 +109,6 @@ void draw_info(const char *flag_name, const char *argv[])
 	/***********************************************/
 
 	// Dont decay into lead pls 😭😭
+	// reply: ok
 	char reset[] = "\033[0m";
-
-
-/*
-printf("Displaying contents of %s:\n", flag_name);
-for (size_t i = 0; i < sizeof(colors) / sizeof(colors[0]); ++i)
-{
-	printf("\t%ld - %d\n", i + 1, colors[i]);
-}
-*/
-/*	// Store the output of 'uname -srm'
-	char uname_info[128];
-	FILE *pipe;
-	pipe = popen("uname -srm", "r");
-	if (pipe == NULL)
-	{
-		perror("popen()");
-		exit(EXIT_FAILURE);
-	}
-	fgets(uname_info, sizeof(uname_info), pipe);
-	uname_info[strlen(uname_info) - 1] = '\0';
-	pclose(pipe);
-
-	// Make sure that the row color is different to the color of the hostname
-	if (flag[0] != flag[1])
-	{
-		chadstr row_color = color256(flag[1], fg);
-	}
-	else
-	{
-		chadstr row_color = color256(flag[2], fg);
-	}
-
-	chadstr reset = str("\033[0m\033[39m");
-
-	printf("\n");
-	for (size_t row = 0; row < strlen(flag); ++row)
-	{
-	}
-*/
-
-	free(entry.key);
-//	free(entry.data);
 }
